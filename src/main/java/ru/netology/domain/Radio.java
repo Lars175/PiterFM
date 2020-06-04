56c7720b9682be560c2178a84c980ed4f649e413
Larisa Karelina <karelina.rss@gmail.com>
2020-06-04 18:28:41 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,60 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentStation;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentVolume;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,32 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void setCurrentStation() {
+    }
+
+    @Test
+    void setMaxStation() {
+    }
+
+    @Test
+    void setMinStation() {
+    }
+
+    @Test
+    void setCurrentVolume() {
+    }
+
+    @Test
+    void setMaxVolume() {
+    }
+
+    @Test
+    void setMinVolume() {
+    }
+}
